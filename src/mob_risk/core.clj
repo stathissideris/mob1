a2a23d93b8ddf3384dca73452bafd2842d9cd86b
Guillaume Alaux <guillaume@alaux.net>
2022-10-13 16:55:54 +0200

    WIP

--- src/mob_risk/core.clj
+++ src/mob_risk/core.clj
@@ -1,15 +1,30 @@
 (ns mob-risk.core)
 
-(def players (atom {}))
+(def state (atom {}))
 
-(def board (atom {}))
+(defn init-players [state player-names]
+  (assoc state :players (zipmap player-names (repeat nil))))
 
-(defn init-players! [player-names]
-  (doseq [n player-names]
-    (swap! players assoc n nil)))
+(defn init-board [state]
+  (assoc state :board {}))
 
-(defn init-board! []
-  (swap! board {}))
+(defn place-unit-allowed? [state player where]
+  (or (nil? (get-in state [:board where :units]))
+      (= player (get-in state [:board where :owner]))))
+
+(defn place-army [state player what where]
+  (if (place-unit-allowed? state player where)
+    (assoc-in state [:board where] {:units what :owner player})
+    (assoc state :error "Not allowed")))
+
+
+
+
+(comment
+  {:phase keyword?
+   :board {:ontario {:units number?
+                     :owner string?}}
+   :error nil})
 
 
 
