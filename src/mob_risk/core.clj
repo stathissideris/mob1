32c107a7694dac4c9aaf5e3d27fa358df21b2a30
Stathis Sideris <stathis@pixelated-noise.com>
2022-10-13 17:17:52 +0300

    WIP

--- src/mob_risk/core.clj
+++ src/mob_risk/core.clj
@@ -1,1 +1,44 @@
 (ns mob-risk.core)
+
+(def players (atom {}))
+
+(def board (atom {}))
+
+(defn init-players! [player-names]
+  (doseq [n player-names]
+    (swap! players assoc n nil)))
+
+(defn init-board! []
+  (swap! board {}))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+(comment
+  (rand-nth ["George" "Guillaume" "Annie" "Konstantinos" "Dinos"]))
